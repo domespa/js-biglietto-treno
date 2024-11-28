7ac6503e836a07e09a9685bdf7ea536282cecf3d
domespa <dumiii1988@gmail.com>
2024-11-28 15:35:43 +0100

    init project

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+    Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+    - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    - va applicato uno sconto del 20% per i minorenni
+    - va applicato uno sconto del 40% per gli over 65.
+    - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.*/
+
+
+// 1 - Chiedo all'utente il numero di chilometri che vuole percorrere   
+// 2 - Chiedo all'utente la sua età  
+// 3 - Prezzo base per km 0.21 €
+// 4 - SE l'utente ha < 18 anni avrà uno sconto del 20%
+//     - ALTRIMENTI SE l'utente ha > 65 anni avrà uno sconto del 40%
+//     ALTRIMENTI l'utente ha >= 18 anni e <= 65 anni pagherà il prezzo per intero
+
+
+    
