a6c9dcdfaeff4c702cecadd2f66683aab49b1750
domespa <dumiii1988@gmail.com>
2024-11-28 16:51:07 +0100

    feat: add variables + calc

--- script.js
+++ script.js
@@ -6,12 +6,30 @@
     - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.*/
 
 
-// 1 - Chiedo all'utente il numero di chilometri che vuole percorrere   
-// 2 - Chiedo all'utente la sua età  
+// 1 - Chiedo all'utente il numero di chilometri che vuole percorrere
+const kiloms = Number(prompt("Quanti chilometri devi percorrere?"));
+console.log(kiloms);   
+// 2 - Chiedo all'utente la sua età
+const userAge = Number(prompt("Quanti anni hai?"));
+console.log(userAge);   
 // 3 - Prezzo base per km 0.21 €
+let basePrice = 0.21 * kiloms;
+console.log(basePrice);   
 // 4 - SE l'utente ha < 18 anni avrà uno sconto del 20%
+let discountPrice
+if (userAge < 18) {
+    discountPrice = basePrice * 0.2;
+    alert("Il prezzo del tuo biglietto è " + (basePrice - discountPrice).toFixed(2) + "€")
 //     - ALTRIMENTI SE l'utente ha > 65 anni avrà uno sconto del 40%
-//     ALTRIMENTI l'utente ha >= 18 anni e <= 65 anni pagherà il prezzo per intero
+} else if (userAge > 65) {
+    discountPrice = basePrice * 0.4;
+    alert("Il prezzo del tuo biglietto è " + (basePrice - discountPrice).toFixed(2) + "€")
+//     ALTRIMENTI l'utente che ha >= 18 anni e <= 65 anni pagherà il prezzo per intero
+} else if (userAge >= 18 && userAge <= 65) {
+    discountPrice = basePrice;
+    alert("Il prezzo del tuo biglietto è " + basePrice.toFixed(2) + "€")
+}
+console.log(discountPrice)
 
 
     
